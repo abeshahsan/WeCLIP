7dcbc80dfb47ee8fce7bebdf4c36e90824db5012
SayemaSaj <130158846+SayemaSaj@users.noreply.github.com>
2024-11-06 18:15:58 +0600

    Update dist_clip_voc.py

--- scripts/dist_clip_voc.py
+++ scripts/dist_clip_voc.py
@@ -235,7 +235,7 @@ def train(cfg):
 
     # Load checkpoint if exists
     start_iter = 0
-    checkpoint_path = os.path.join("/content/drive/MyDrive/WEclip-ckpt", "WeCLIP_model_iter_12000.pth")
+    checkpoint_path = os.path.join("/content/drive/MyDrive/WEclip-ckpt", "WeCLIP_model_iter_13800.pth")
     
     if os.path.exists(checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
