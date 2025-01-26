85558bfea1dc1f637a10ac6781d367d5ff6fd9a3
abeshahsan <abeshahsan2002@gmail.com>
2025-01-27 01:07:08 +0600

    Add debug print statements for feature and attention shapes in SwinTransformer

--- UniCL/model/image_encoder/swin_transformer.py
+++ UniCL/model/image_encoder/swin_transformer.py
@@ -603,6 +603,12 @@ class SwinTransformer(nn.Module):
                 x = x[-1]
             
             if require_all_fts:
+                for x in x_all:
+                    print(x.shape)
+                print()
+                for attn in attn_all:
+                    print(attn.shape)
+
                 for i in range(len(x_all)):
                     if x_all[i].shape[-1] == 192:
                       x_all[i] = self.proj_1(x_all[i])
