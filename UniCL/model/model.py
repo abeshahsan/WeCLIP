ab95631fff80b9fd7cfa383e818ec36f8f04811c
Ani445 <102604070+Ani445@users.noreply.github.com>
2025-03-08 22:19:56 +0600

    Comment out last layer feature projection and normalization in UniCLModel

--- UniCL/model/model.py
+++ UniCL/model/model.py
@@ -189,9 +189,9 @@ class UniCLModel(nn.Module):
         self.original_last_attn_weight = attn[-1].clone()
         
         #project last layer fts from 1024 to 512
-        x[-1] = x[-1] @ self.image_projection
-        if norm:
-            x[-1] = x[-1]/x[-1].norm(dim = -1, keepdim = True)
+        # x[-1] = x[-1] @ self.image_projection
+        # if norm:
+        #     x[-1] = x[-1]/x[-1].norm(dim = -1, keepdim = True)
 
 
         # for i, fts in enumerate(x):
